df7704a60ce575cc1ba92bc9942543124e06126f
George <georgewoodall82@gmail.com>
2024-07-15 20:11:25 +0100

    fix whitespace mess

--- src/main/java/org/naurd/media/jsymphonic/toolBox/JSymphonicTreeCellRenderer.java
+++ src/main/java/org/naurd/media/jsymphonic/toolBox/JSymphonicTreeCellRenderer.java
@@ -25,74 +25,73 @@
  *
  */
 
- package org.naurd.media.jsymphonic.toolBox;
+package org.naurd.media.jsymphonic.toolBox;
 
- import java.awt.Color;
- import java.awt.Component;
- import javax.swing.ImageIcon;
- import javax.swing.JTree;
- import javax.swing.UIManager;
- import org.danizmax.jsymphonic.toolkit.JSymphonicMutableTreeNode;
- import javax.swing.tree.DefaultTreeCellRenderer;
- import org.naurd.media.jsymphonic.title.Title;
- 
- import com.pipasoft.jsymphonic.ResourceLoader;
- 
- /**
-  *
-  * @author skiron
-  * @author danizmax - Daniel Žalar (danizmax@gmail.com) - add forground colors
-  */
- public class JSymphonicTreeCellRenderer extends DefaultTreeCellRenderer {
-     
-     void doColorIcons(){
-     }
-     
-     @Override
-     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-         super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
- 
- 
-         if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_USB ) { 
-            setIcon(ResourceLoader.getIcon("device.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_ALBUM ) { 
-            setIcon(ResourceLoader.getIcon("album.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_ARTIST ) {
-            setIcon(ResourceLoader.getIcon("artist.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_GENRE ) {
-            setIcon(ResourceLoader.getIcon("genre.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_PLAYLIST ) {
-            setIcon(ResourceLoader.getIcon("playlist.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_TITLE ) {
-            setIcon(ResourceLoader.getIcon("title.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_FOLDER ) {
-            setIcon(ResourceLoader.getIcon("folder.png"));
-         }
-         else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_AUDIOFILE ) {
-            setIcon(ResourceLoader.getIcon("audiofile.png"));
-         }
-         else {
-            setIcon(ResourceLoader.getIcon("default.png"));
-         }
-         
-         //set the action color
-         if( ((JSymphonicMutableTreeNode)value).getNodeAction() == Title.TO_DELETE) {
-             this.setForeground(Color.RED);
-         }else if( ((JSymphonicMutableTreeNode)value).getNodeAction() ==Title.TO_EXPORT ){
-             this.setForeground(Color.BLUE);
-         }else if( (((JSymphonicMutableTreeNode)value).getNodeAction() == Title.TO_IMPORT ) || ( ((JSymphonicMutableTreeNode)value).getNodeAction() ==Title.TO_ENCODE )  || ( ((JSymphonicMutableTreeNode)value).getNodeAction() ==Title.TO_DECODE ) ){
-             this.setForeground(Color.GREEN);
-         }else if( ((JSymphonicMutableTreeNode)value).getNodeAction() == Title.ON_DEVICE ) {
-             this.setForeground(UIManager.getColor(Color.BLACK));
-         }
- 
-         return this;
-     }
- }
- 
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.UIManager;
+import org.danizmax.jsymphonic.toolkit.JSymphonicMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import org.naurd.media.jsymphonic.title.Title;
+
+import com.pipasoft.jsymphonic.ResourceLoader;
+
+/**
+ *
+ * @author skiron
+ * @author danizmax - Daniel Žalar (danizmax@gmail.com) - add forground colors
+ */
+public class JSymphonicTreeCellRenderer extends DefaultTreeCellRenderer {
+    
+    void doColorIcons(){
+    }
+    
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+
+
+        if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_USB ) { 
+           setIcon(ResourceLoader.getIcon("device.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_ALBUM ) { 
+           setIcon(ResourceLoader.getIcon("album.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_ARTIST ) {
+           setIcon(ResourceLoader.getIcon("artist.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_GENRE ) {
+           setIcon(ResourceLoader.getIcon("genre.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_PLAYLIST ) {
+           setIcon(ResourceLoader.getIcon("playlist.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_TITLE ) {
+           setIcon(ResourceLoader.getIcon("title.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_FOLDER ) {
+           setIcon(ResourceLoader.getIcon("folder.png"));
+        }
+        else if( ((JSymphonicMutableTreeNode)value).getType() == JSymphonicMutableTreeNode.TYPE_AUDIOFILE ) {
+           setIcon(ResourceLoader.getIcon("audiofile.png"));
+        }
+        else {
+           setIcon(ResourceLoader.getIcon("default.png"));
+        }
+        
+        //set the action color
+        if( ((JSymphonicMutableTreeNode)value).getNodeAction() == Title.TO_DELETE) {
+            this.setForeground(Color.RED);
+        }else if( ((JSymphonicMutableTreeNode)value).getNodeAction() ==Title.TO_EXPORT ){
+            this.setForeground(Color.BLUE);
+        }else if( (((JSymphonicMutableTreeNode)value).getNodeAction() == Title.TO_IMPORT ) || ( ((JSymphonicMutableTreeNode)value).getNodeAction() ==Title.TO_ENCODE )  || ( ((JSymphonicMutableTreeNode)value).getNodeAction() ==Title.TO_DECODE ) ){
+            this.setForeground(Color.GREEN);
+        }else if( ((JSymphonicMutableTreeNode)value).getNodeAction() == Title.ON_DEVICE ) {
+            this.setForeground(UIManager.getColor(Color.BLACK));
+        }
+
+        return this;
+    }
+}
